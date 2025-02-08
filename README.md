e817d6b56e422b376ba446430e3a4e3b4d41397f
Darius <dariusz.praczuk@gmail.com>
2025-02-08 23:57:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Samouk Project 1
+
+## Technologie
+
+- React
+- TypeScript
+- Vite
+- Sass
+- Tanstack Query
+
+## Struktura projektu
+
+Projekt wykorzystuje https://github.com/alan2207/bulletproof-react
+
+## Jak uruchomić projekt
+
+1. Sklonuj repozytorium:
+
+```
+git clone https://github.com/twoje-repozytorium/samouk-project.git
+```
+
+2. Przejdź do katalogu projektu:
+
+```bash
+cd samouk-project
+```
+
+3. Zainstaluj zależności:
+
+```bash
+pnpm install
+```
+
+4. Uruchom aplikację:
+
+```bash
+pnpm run dev
+```
---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import viteReact from '@vitejs/plugin-react';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
+});
